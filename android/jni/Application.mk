61f0e2ddd55812eda5b796b4c3afe26aeb90063b
tuxafgmur <tuxafgmur@users.noreply.github.com>
2015-08-24 12:55:11 +0200

    Prepare android compilation

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,6 +1,6 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
-APP_OPTIM := debug
-#APP_OPTIM := release
+#APP_OPTIM := debug
+APP_OPTIM := release
 #APP_ABI := armeabi 
 APP_ABI := armeabi-v7a
 #APP_ABI := mips 
