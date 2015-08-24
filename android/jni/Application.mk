2ad3dfdf75ba68d3baaa3f496b6fb6a7b248cac0
tuxafgmur <tuxafgmur@users.noreply.github.com>
2015-08-25 01:13:55 +0200

    New compilation adjustments

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,18 +1,9 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
-#APP_OPTIM := debug
-APP_OPTIM := release
-#APP_ABI := armeabi 
+APP_OPTIM := debug
+#APP_OPTIM := release
 APP_ABI := armeabi-v7a
-#APP_ABI := mips 
-#APP_ABI := x86
-#APP_ABI := x86_64
-#APP_ABI := arm64-v8a
-#APP_ABI := armeabi armeabi-v7a mips x86
-#APP_ABI := x86_64 arm64-v8a
-#APP_ABI := armeabi armeabi-v7a mips x86 x86_64 arm64-v8a
-#armeabi-v7a mips
-#mips mips-r2
-#x86
+#APP_ABI := armeabi armeabi-v7a
+#APP_ABI := armeabi armeabi-v7a arm64-v8a
+#APP_ABI := all
 #armeabi-v7a
-APP_PLATFORM := android-8
-#APP_PLATFORM := android-3
+APP_PLATFORM := android-14
